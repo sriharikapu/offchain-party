873445759341792dfe949ccc596ce2cef64c0da1
Igor Barbashin <igor.barbashin@gmail.com>
2019-02-16 09:44:40 -0700

    Add Experience page

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import { DrizzleProvider } from 'drizzle-react';
-import { LoadingContainer } from 'drizzle-react-components';
+
 import { Router } from '@reach/router';
 
-import drizzleOptions from './drizzleOptions';
 import Landing from './pages/Landing';
+import Experience from './pages/Experience';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Landing path="/" />
-        <div path="/1">
-          <DrizzleProvider options={drizzleOptions}>
-            <LoadingContainer />
-          </DrizzleProvider>
-        </div>
+        <Experience path="/1" />
       </Router>
     );
   }
