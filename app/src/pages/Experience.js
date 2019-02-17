3cab7a732884903ab5f21acf2eebf4d3848c89f1
Igor Barbashin <igor.barbashin@gmail.com>
2019-02-17 07:08:49 -0700

    Adapt data from smart contract

--- app/src/pages/Experience.js
+++ app/src/pages/Experience.js
@@ -3,6 +3,7 @@ import { DrizzleProvider } from 'drizzle-react';
 import { LoadingContainer } from 'drizzle-react-components';
 import drizzleOptions from '../drizzleOptions';
 import styled from 'styled-components';
+import { convert } from 'ethereumjs-units';
 
 import CrackadoomPlayer from '../components/CrackadoomPlayer';
 import Layout from '../components/Layout';
@@ -13,7 +14,25 @@ import Description from '../components/Description';
 
 const Container = styled.div``;
 
-const Experience = () => {
+const Experience = ({ Auction721, web3 }) => {
+  function adaptData(raw) {
+    if (!raw || !raw['0x0'] || !web3) {
+      return {};
+    }
+    const value = raw['0x0'].value;
+
+    if (value) {
+      return {
+        price: convert(value.price, 'wei', 'eth'),
+        winningAddress: value.winning,
+        endsAtBlock: value.endsAtBlock
+      };
+    }
+  }
+
+  const auction = adaptData(Auction721.auction);
+
+  console.log({ auction });
   const [loggedIn, setLoggedIn] = useState(true);
   const [auctionEnded, setAuctionEnded] = useState(false);
   const [isTokenOwner, setTokenOwner] = useState(false);
@@ -23,7 +42,7 @@ const Experience = () => {
       return (
         <Container>
           <BannerImage />
-          <AuctionWidget />
+          <AuctionWidget auction={auction} />
         </Container>
       );
     }
